de9a81b7d73864488959c60f326b26b27493343a
Enzo Boulin <enzoboulin@gmail.com>
2025-12-21 23:54:26 +0100

    typo

--- tp_tokens/token.py
+++ tp_tokens/token.py
@@ -6,15 +6,9 @@ from tokenizers.pre_tokenizers import Whitespace
 from tokenizers.trainers import BpeTrainer
 
 
-class Token(str):
-    """Représente un token (mot, sous-mot, caractère, etc.)"""
-
-    pass
-
-
 def main(
     filepath: str = "data/civil_sentences.txt",
-    savepath: Optional[str] = "models/civil_tokenize.json",
+    savepath: Optional[str] = "models/civil_tokenizer.json",
 ):
     tokenizer = Tokenizer(BPE())
     tokenizer.pre_tokenizer = Whitespace()
@@ -26,11 +20,12 @@ def main(
     tokenizer.train(files, trainer)
     if savepath is not None:
         tokenizer.save(savepath)
+        print(f"Tokenizer saved to {savepath}")
 
 
 if __name__ == "__main__":
     main()
-    tokenizer = Tokenizer.from_file("models/civil_tokenize.json")
+    tokenizer = Tokenizer.from_file("models/civil_tokenizer.json")
     output = tokenizer.encode("La question préjudicielle est posée devant la CJUE.")
     print(output.tokens)
     print(output.ids)
